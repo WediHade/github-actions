1dae1ad97a5e83d1e5f2e25737f457a2502c8cd1
WediHade <michealehadera048@gmail.com>
2023-10-25 22:12:38 -0500

    files added

---
+++ src/main/java/com/mike/githubcicdactions/GithubCiCdActionsApplication.java
@@ -0,0 +1,19 @@
+package com.mike.githubcicdactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubCiCdActionsApplication {
+    @GetMapping("/welcome")
+    public String welcome() {
+       return "Welcome to Github Action program";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(GithubCiCdActionsApplication.class, args);
+    }
+
+}
